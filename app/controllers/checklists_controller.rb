658a739364817aad3813bad72d02bebf3404b7c1
Macrish <tatianadavydiuk@gmail.com>
2019-09-10 09:25:16 -0700

    add drop-down menu & add icons

--- app/controllers/checklists_controller.rb
+++ app/controllers/checklists_controller.rb
@@ -2,7 +2,7 @@ class ChecklistsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :handle_record_not_found
 
   def index
-  	@checklists = current_user.checklists.all
+  	@checklists = Checklist.all
   end
 
   def show
